c7e8487582b2a7de53e1685112366d3122ff0604
web3sea <yanji.notification@gmail.com>
2025-08-02 20:02:09 +0700

    feat(ui): :sparkles: integrate chatbot hubspot

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -23,6 +24,7 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    Sitemap({ hostname: 'https://coraltriangle.io/' }),
   ].filter(Boolean),
   resolve: {
     alias: {
